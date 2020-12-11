00fc9ec47ae2143b893b7c564c85c5f8ec9eee18
Matthew Helmus <mrhelmus@temple.edu>
2020-12-11 08:20:18 -0500

    edits
    started making edits to the herp function to better resemble what was done for beetles so that the two tables are more or less the same.

--- sandbox/data_herp_bycatch.R
+++ sandbox/data_herp_bycatch.R
@@ -1,11 +1,47 @@
-#' Herpetofauna pitfall trap bycatch data collected by NEON
+#' Vertebrate Bycatch sampled from pitfall traps
 #'
 #' This dataset was derived from [NEON data portal](https://data.neonscience.org) with data product ID 'DP1.10022.001'. Details about this data product can be found at <https://data.neonscience.org/data-products/DP1.10022.001>.
 #'
 #' The detailed design of NEON plant survey can be found in [Hoekman et al. 2017](https://esajournals.onlinelibrary.wiley.com/doi/full/10.1002/ecs2.1744).
 
-#' Here, we:
 #'
+#' This dataset was derived from [NEON data portal](https://data.neonscience.org) with data product ID 'DP1.10022.001'. Details about this data product can be found at <https://data.neonscience.org/data-products/DP1.10022.001>.
+#'
+#' To process data we:
+#' 1. Remove all non-carabid bycatch samples.
+#' 2. Use the most expert taxonomy when available.
+#' 3. Update abundances based on new taxonomy.
+#' 4. Create a boutID that identifies all trap collection events at a site in the same bout (replacing eventID).
+#' 5. Update collectDate to reference the most common collection day in a bout, maintaining one collectDate per bout.
+#' 6. Create a new `trappingDays` column for the number of days a trap was set before being collected.
+#' 7. Correct trap days to account for entries where the trap set date was not updated based on a previous collection.
+#'
+#' @note Details of locations (e.g. latitude/longitude coordinates can be found in [neon_locations]).
+#'
+#' @format A data frame (also a tibble) with the following columns:
+#'
+#' - `sampleID`: Identifier for sample.
+#' - `siteID`: NEON site code.
+#' - `plotID`: Plot identifier (NEON site code_XXX).
+#' - `namedLocation`: Name of the measurement location in the NEON database.
+#' - `trapID`: Identifier for trap.
+#' - `setDate`: Date that trap was set.
+#' - `collectDate`: Date of the collection event.
+#' - `trappingDays`: Decimal days between trap setting and collecting events.
+#' - `nativeStatusCode`: The process by which the taxon became established in the location.
+#' 'A': Presumed absent, due to lack of data indicating a taxon's presence in a given location;
+#' 'N': Native; 'I': Introduced; 'UNK': Status unknown.
+#' - `identificationSource`: Source of identification.
+#' - `taxonID`: Species code, based on one or more sources.
+#' - `taxonRank`: The lowest level taxonomic rank that can be determined for the individual or specimen.
+#' - `scientificName`: Scientific name, associated with the taxonID. This is the name of the lowest level taxonomic rank that can be determined.
+#' - `count`: Number of individuals. `NA` represents no beetles catche in the trap.
+#'
+#' @author Kari Norman
+#'
+"data_beetle"
+
+
 STOPPED HERE 
 #' 1. Removed 1 m^2 data with `targetTaxaPresent = N`
 #' 2. Removed rows without plotID, subplotID, boutNumber, endDate, and/or taxonID
@@ -70,23 +106,23 @@ include_zeros <- FALSE # Still need to figure out the zeros... is there a NULL t
 my_dpid <- 'DP1.10022.001' # beetle dpid to get herp bycatch
 my_site_list <- c('BART') # start with just one 
 
-all_tabs <- neonUtilities::loadByProduct(
+bycatch_raw <- neonUtilities::loadByProduct(
   dpID = my_dpid,
   site = my_site_list,
-  check.size = FALSE,
-  package = "expanded") #must use the expanded to get bycatch
+  check.size = FALSE#,
+#  package = "expanded"
+  ) # if there is a problem in the later code then use expanded
 
 # bet_fielddata has sampleID for all samples that were taken this can be used to 
 # add zeros back in the herp data
 # includes location and lat long data
-bet_fielddata <- all_tabs$bet_fielddata
+bet_fielddata <- bycatch_raw$bet_fielddata
 
 # get actual sampling data
-herp_sorting<- all_tabs$bet_sorting %>% filter(sampleType == "vert bycatch herp")
+herp_sorting<- bycatch_raw$bet_sorting %>% filter(sampleType == "vert bycatch herp")
 
 index <- intersect(herp_sorting$sampleID, bet_fielddata$sampleID)
 
-#inv_taxonomyProcessed <- all_tabs$bet_expertTaxonomistIDProcessed
 herp_taxonomyProcessed <- herp_sorting %>% 
   dplyr::select(taxonID, scientificName, taxonRank) %>% 
   unique()
